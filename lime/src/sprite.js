9e26a5d30012e375e7ab36ee5ce7769eacf27ea7
Anders Lauritsen <lauritsen@komogvind.dk>
2011-09-26 16:03:18 +0200

    Fixes Chrome bug, where style is appended "border-width: initial; border-color: initial;". Not a pretty fix.

--- lime/src/sprite.js
+++ lime/src/sprite.js
@@ -120,7 +120,7 @@ lime.Renderer.DOM.SPRITE.draw = function(el) {
     if (goog.isDefAndNotNull(this.stroke_)) {
         this.stroke_.setDOMStyle(el, this);
     } else {
-        el.style.border='none';
+        el.style['border'] = '';
     }
 };
 
